dc460e0e99dc81074b9409fedac4f6ba1505bf15
nator junior <natorapps@gmail.com>
2020-07-16 16:32:34 -0300

    Enviroment jupyter notebook exec

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM python:3.6
+ADD . /jupyter_notebook
+WORKDIR /jupyter_notebook
+RUN pip install -r requirements.txt
+RUN pip install notebook
+RUN apt-get update
+RUN apt-get install nano
+RUN python3 down_data.py
+
+ENTRYPOINT ["python"]
+CMD ["jupyter-notebook"]
---
+++ down_data.py
@@ -0,0 +1,6 @@
+import os
+os.system('wget https://pjreddie.com/media/files/yolov3.weights')
+os.system('cp yolov3.weights 3\ -\ Model\ Deployment\ \(WebDocker\)/flask_code/ml')
+os.system('cp yolov3.weights 3\ -\ Model\ Deployment\ \(WebDocker\)')
+os.system('cp yolov3.weights 4\ -\ Object\ detection\ com\ deep\ learning/ml/')
+os.system('cp yolov3.weights 4\ -\ Object\ detection\ com\ deep\ learning/')
